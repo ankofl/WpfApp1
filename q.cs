28334295fe871382951d081876fa7a1d691f4feb
ankofl <ankofl@yandex.ru>
2023-10-11 16:16:02 +0300

    q

---
+++ q.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using WpfApp1.Geometry;
+
+namespace WpfApp1
+{
+	
+}
